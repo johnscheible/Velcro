5e9f4062b46ff58a0b3bfeca24c6335f4551963f
John Scheible <johnscheible@gmail.com>
2013-07-13 18:14:56 -0500

    Arduino code added

---
+++ sketchbook/pulse.ino
@@ -0,0 +1,53 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13. 
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead(). 
+ 
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ * Note: because most Arduinos have a built-in LED attached 
+ to pin 13 on the board, the LED is optional.
+ 
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://arduino.cc/en/Tutorial/AnalogInput
+ 
+ */
+
+int pulsePin = A0;    // select the input pin for the potentiometer
+//int blinkPin = 13;                // pin to blink led at each beat
+int Signal = 0;  // variable to store the value coming from the sensor
+//int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+//int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  //sensorValue = analogRead(sensorPin);    
+  
+  //Serial.println(sensorValue);
+ 
+  getPulse();
+  
+  Serial.println("ALSKDJFKLJASFJKL");
+  
+  delay(1);                  
+}
